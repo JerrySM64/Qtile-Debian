87c815c5b0e27336564b1e8b4ce18a4e7790165c
Jerry <42114389+JerrySM64@users.noreply.github.com>
2023-06-20 01:55:39 +0200

    Embedded the desktop file into the script.

--- qtile-setup.sh
+++ qtile-setup.sh
@@ -4,7 +4,8 @@
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 
 # Install the full Python 3 Suite, pip, git and all Dependencies
-sudo apt update && sudo apt install python3-full python3-pip libpangocairo-1.0-0 python3-cffi python3-xcffib git -y
+sudo apt update
+sudo apt install python3-full python3-pip libpangocairo-1.0-0 python3-cffi python3-xcffib git -y
 
 # Install cairocffi using pip
 pip install --no-cache-dir cairocffi
@@ -17,5 +18,21 @@ cd qtile
 pip install .
 
 # Xsessions Desktop file
-sudo mv qtile.desktop /usr/share/xsessions/
-sudo chmod +x /usr/share/xsessions/qtile.desktop
+cat > ./temp << "EOF"
+[Desktop Entry]
+Name=Qtile
+Comment=Qtile Session
+Type=Application
+Keywords=wm;tiling
+EOF
+
+sudo cp ./temp /usr/share/xsessions/qtile.desktop
+sudo echo "Exec=/home/$USER/.local/bin/qtile start" | sudo tee -a /usr/share/xsessions/qtile.desktop
+
+# Cleanup
+rm ./temp
+
+# Done
+echo ""
+echo "Installation complete!"
+echo ""
